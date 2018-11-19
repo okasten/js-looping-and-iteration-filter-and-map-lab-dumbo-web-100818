21fbe0e937a1432db530ffe998d58fa7fa4a76a4
Olivia Kasten <odkasten@gmail.com>
2018-11-19 03:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function driversWithRevenueOver(drivers, revenue){
 }
 
 function driverNamesWithRevenueOver(drivers, revenue){
-  return driversWithRevenueOver(drivers, revenue).map(function (name){
-    
+  return driversWithRevenueOver(drivers, revenue).map(function (driver){
+    return driver.name;
   })
 }
